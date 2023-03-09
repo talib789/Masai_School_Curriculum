8e95a0c910ca659755ba0158ad5eaffb580a1b77
Md Talib Ansari <95065465+talib789@users.noreply.github.com>
2023-03-09 23:10:57 +0530

    Update github.js

--- Unit-07/EXPRESS AUTHORIZATION - PASSPORT/strategies/github.js
+++ Unit-07/EXPRESS AUTHORIZATION - PASSPORT/strategies/github.js
@@ -7,12 +7,12 @@ const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 
  passport.use(
     new GithubStrategy(
-      {
+       {
         clientID: GITHUB_CLIENT_ID,
         clientSecret: GITHUB_CLIENT_SECRET,
         callbackURL: 'http://localhost:3000/auth/github/callback', 
         passReqToCallback: true,
-       },
+        },
       function(req, accessToken, refreshToken, profile, done) {
 
         profile = profile._json;
